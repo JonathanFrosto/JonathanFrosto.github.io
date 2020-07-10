9fea6cd3b1b2d521b66bfce2595edec8e3468f7d
Jonathan Anthony <jonataanthony@gmail.com>
2020-07-10 18:34:58 -0300

    Horizontal scroll done

--- app.js
+++ app.js
@@ -1,20 +1,52 @@
-const plus = $('#plus');
-const minus = $('#minus');
+// Up/down navigation
+$('html, body').animate({scrollTop: $('#section1').offset().top}, 1000);
+
+const up = $('#up');
+const down = $('#down');
 let count = 1;
 
-// $('html, body').animate({scrollTop: $('#section1').offset().top}, 1000);
-
-minus.click(function(){
+down.click(function(){
     if ( count < 4){
         count++;
-        console.log(count)
         $('html, body').animate({scrollTop: $('#section' + count).offset().top}, 1000);
     }
 });
 
-plus.click(function(){
+up.click(function(){
     if ( count > 1){
         count--;
         $('html, body').animate({scrollTop: $('#section' + count).offset().top}, 1000);
     }
 });
+
+// horizontal scroll
+const simpleBar = new SimpleBar(document.getElementById('items'));
+const slider = document.getElementById('items');
+let isDown = false;
+let startX, scrollLeft;
+
+slider.addEventListener('mousedown',(e) => {
+    isDown = true;
+    startX = e.pageX - slider.offsetLeft;
+    // scrollLeft = slider.scrollLeft;
+    scrollLeft = simpleBar.getScrollElement().scrollLeft;
+})
+
+slider.addEventListener('mouseleave',() => {
+    isDown = false;
+})
+
+slider.addEventListener('mouseup',() => {
+    isDown = false;
+})
+
+slider.addEventListener('mousemove',(e) => {
+    if(!isDown) return; // Parar a função
+    e.preventDefault();
+    const x = e.pageX - slider.offsetLeft;
+    const walk = x - startX;
+    simpleBar.getScrollElement().scrollLeft = scrollLeft - walk ;
+    // slider.scrollLeft = scrollLeft - walk;
+    // console.log({startX,x, walk}); 
+})
+
